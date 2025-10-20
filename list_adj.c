bf8f33eeecdf1c8f45b422b52bba1cefbacb763d
julie <julie.sieux@efrei.net>
2025-10-20 17:54:24 +0200

    Fonnctions utiles à l'implémentation d'une liste/liste d'adjacence

--- list_adj.c
+++ list_adj.c
@@ -3,3 +3,46 @@
 //
 
 #include "list_adj.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+t_list CreateEmptyList(){
+  t_list list;
+  list.head = NULL;
+  return list;
+}
+
+void AddCellToList(t_list *list, t_cell *new_cell){
+  t_cell *cell;
+  cell = list->head;
+  while (cell->next != NULL){
+    cell = cell->next;
+  }
+  cell->next = new_cell;
+}
+
+void PrintList(t_list list){
+  printf("[head @]");
+
+  t_cell *cell;
+  cell = list.head;
+  while (cell != NULL){
+    printf(" -> (%d, %f)", cell->summit_arrival, cell->proba);
+    cell = cell->next;
+  }
+}
+
+t_adj *CreateEmptyAdj(int taille){
+  t_adj *adj = malloc(sizeof(t_adj));
+  adj->lenght = taille;
+  adj->leaving_edge = malloc(taille*sizeof(t_list));
+  return adj;
+}
+
+void PrintAdj(t_adj adj){
+  for (int i = 0; i < adj.lenght; i++){
+    printf("Liste pour le sommet %d :", i);
+    PrintList(adj.leaving_edge[i]);
+    printf("\n");
+  }
+}
--- list_adj.h
+++ list_adj.h
@@ -4,5 +4,25 @@
 
 #ifndef LIST_ADJ_H
 #define LIST_ADJ_H
+#include "Cell.h"
+
+// Définition des types liste et liste d'adjacence
+
+typedef struct s_list {
+  t_cell *head;
+} t_list;
+
+typedef struct s_adj {
+  int lenght;
+  t_list *leaving_edge;
+} t_adj;
+
+// Définition des fonctions utiles pour les opérations sur les listes
+
+t_list CreateEmptyList();
+void AddCellToList(t_list *list, t_cell *cell);
+void PrintList(t_list list);
+t_adj *CreateEmptyAdj(int taille);
+void PrintAdj(t_adj adj);
 
 #endif //LIST_ADJ_H
