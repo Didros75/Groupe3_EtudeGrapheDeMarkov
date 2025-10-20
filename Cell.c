5e83b1a49c52d024fb85c9354b738b30a5a155f0
Arthur <arthur.morineaux@efrei.net>
2025-10-20 17:35:56 +0200

    ajout dde la structure t_cell et de la fonction createCell

--- Cell.c
+++ Cell.c
@@ -1,5 +1,13 @@
 //
 // Created by julie on 20/10/2025.
 //
-
+#include <stdlib.h>
 #include "Cell.h"
+
+t_cell *createCell(int summit_arrival , float proba){
+    t_cell *new = (t_cell *)malloc(sizeof(t_cell));
+    new->summit_arrival = summit_arrival;
+    new->proba = proba;
+    new->next = NULL;
+    return new;
+}
--- Cell.h
+++ Cell.h
@@ -5,4 +5,13 @@
 #ifndef CELL_H
 #define CELL_H
 
+typedef struct s_cell {
+    int summit_arrival;
+    float proba;
+    struct s_cell *next;
+} t_cell;
+
+
+t_cell *createCell(int summit_arrival , float proba);
+
 #endif //CELL_H
