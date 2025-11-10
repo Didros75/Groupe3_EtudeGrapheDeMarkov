4d23f0e204a362b07dc4a3c4f807d59c702f7ed2
julie <julie.sieux@efrei.net>
2025-11-10 18:57:28 +0100

    Début Etape 2 Partie 2 : création des structures et définition de la fonction hasse

--- hasse.h
+++ hasse.h
@@ -1,7 +1,21 @@
 #ifndef __HASSE_H__
 #define __HASSE_H__
 
+#include "list_adj.h"
+#define T_MAX 100
+
+typedef struct s_link {
+  int from;
+  int to;
+} t_link;
+
+typedef struct s_link_array {
+    t_link links[T_MAX];
+    int log_size;
+} t_link_array;
+
 void removeTransitiveLinks(t_link_array *p_link_array);
+void hasse(int * array_class, t_adj adj, t_link_array *link_array);
 
 /**
  * @brief Creates a link array from the given partition and graph.
